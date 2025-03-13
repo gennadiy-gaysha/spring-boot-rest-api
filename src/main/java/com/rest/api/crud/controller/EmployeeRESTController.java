89b90b65051ff25d07b0eb4b8f52c4641665fbaa
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2025-03-13 16:45:18 +0100

    feat: add mapping for PUT http request to update existing employee

--- src/main/java/com/rest/api/crud/controller/EmployeeRESTController.java
+++ src/main/java/com/rest/api/crud/controller/EmployeeRESTController.java
@@ -14,35 +14,41 @@ public class EmployeeRESTController {
     private final EmployeeService employeeService;
 
     @Autowired // one constructor - annotation is not obligatory
-    public EmployeeRESTController(EmployeeService employeeService){
+    public EmployeeRESTController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
     // expose "/employees" and return a list of employees
     @GetMapping("/employees")
-    public List<Employee> findAll(){
+    public List<Employee> findAll() {
         return employeeService.findAll();
     }
 
     // Get employee by their id
     // add mapping for GET /employees/{employeeId}
     @GetMapping("/employees/{employeeId}")
-    public Employee findById(@PathVariable int employeeId){
+    public Employee findById(@PathVariable int employeeId) {
         return employeeService.findById(employeeId);
     }
 
     // Create new employee
-    // add mapping for POST /employees
+    // add mapping for POST /employees - create new employee
     @PostMapping("/employees")
     // Tells Spring to convert the incoming JSON request body into an Employee object.
     // This allows the method to accept employee data from the client.
-    Employee addEmployee(@RequestBody Employee theEmployee){
+    Employee addEmployee(@RequestBody Employee theEmployee) {
         // This ensures that a new employee is created instead of updating an existing one.
         //In databases with auto-generated primary keys, setting id = 0 signals that this
         // is a new record.
         theEmployee.setId(0);
-        Employee dbEmployee = employeeService.save(theEmployee);
-        return dbEmployee;
+        return employeeService.save(theEmployee);
+
+    }
+
+    // add mapping for PUT /employees - update existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+        return employeeService.save(theEmployee);
     }
 
 }
