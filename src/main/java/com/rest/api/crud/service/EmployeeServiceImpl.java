975c91644199a3bb13e80fc29f45c2e8445016d9
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2025-03-13 14:59:23 +0100

    feat: : add corresponding find, create/update and delete methods to the Service layer

--- src/main/java/com/rest/api/crud/service/EmployeeServiceImpl.java
+++ src/main/java/com/rest/api/crud/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.rest.api.crud.service;
 
 import com.rest.api.crud.dao.EmployeeDAO;
 import com.rest.api.crud.entity.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,8 +15,29 @@ public class EmployeeServiceImpl implements EmployeeService {
         this.employeeDAO = employeeDAO;
     }
 
+    @Transactional
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
     }
+
+    @Transactional
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
+    }
+
+
 }
