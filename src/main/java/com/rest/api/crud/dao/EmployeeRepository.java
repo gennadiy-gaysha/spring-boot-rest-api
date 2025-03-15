c1c8b2124c8300a1a96928412fb7eafdbad10261
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2025-03-15 10:11:47 +0100

    feat: add Data REST cinfiguration for base-path and pagination

--- src/main/java/com/rest/api/crud/dao/EmployeeRepository.java
+++ src/main/java/com/rest/api/crud/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.rest.api.crud.dao;
 
 import com.rest.api.crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
